e3e69ff5fff9d6a4938229f33bd9b3eff6f5cc4a
Tony Vitez <amvitez@gmail.com>
2024-10-28 22:25:08 -0400

    Update scripts.js

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -9,15 +9,3 @@ customElements.define('test-summary',
         .appendChild(template.cloneNode(true));
   }
 });
-
-customElements.define('test-details',
-  class extends HTMLElement {
-    constructor() {
-      super();
-      const template = document
-        .getElementById('test-details-template')
-        .content;
-      const shadowRoot = this.attachShadow({mode: 'open'})
-        .appendChild(template.cloneNode(true));
-  }
-});
