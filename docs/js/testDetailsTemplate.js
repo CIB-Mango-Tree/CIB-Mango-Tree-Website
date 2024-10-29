ee02ec62e972cba4d1645be6f9cff5a893992c53
Tony Vitez <amvitez@gmail.com>
2024-10-28 23:12:39 -0400

    Update testDetailsTemplate.js

--- docs/js/testDetailsTemplate.js
+++ docs/js/testDetailsTemplate.js
@@ -1,5 +1,5 @@
-let templateHTML = 
-	`<template id="test-details-template">
+const templateHTML = `
+	<template id="test-details-template">
 			<style>
 				@import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css";
 				@import url("./docs/css/styles.css");
@@ -66,7 +66,8 @@ let templateHTML =
 					</div>
 				</div>
 			</div>
-		</template>`;
+		</template>
+  `
 
 const templatePlaceholder = document.getElementById("template-placeholder");
 templatePlaceholder.outerHTML = templateHTML;
