ad9a9c6088633463580ae590d4e8acb75e3a9b58
Tony Vitez <amvitez@gmail.com>
2024-10-29 09:14:08 -0400

    Update testDetailsTemplate.js

--- docs/js/testDetailsTemplate.js
+++ docs/js/testDetailsTemplate.js
@@ -1,4 +1,4 @@
-const templateHTML = 
+let templateHTML = 
 	`<template id="test-details-template">
 			<style>
 				@import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css";
